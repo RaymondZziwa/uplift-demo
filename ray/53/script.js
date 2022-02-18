c7e40fd1c5b95e90484573dfdaa91fe10f47ede1
Zziwa Raymond Ian <46450094+RaymondZziwa@users.noreply.github.com>
2022-02-18 11:17:49 +0300

    Add files via upload

--- ray/53/script.js
+++ ray/53/script.js
@@ -20,27 +20,31 @@ const formulaCelciusToKelvin = (i) =>{
      return result2;
 }
 
-//check function
+//check function  imgs/Activity-9_0000s_0004s_0000s_0000_excellent.png
 const check = () =>{
     if(ansOne == formulaCelciusToKelvin(53)){
+	  document.getElementById("imgstatus1").src="imgs/Activity-9_0000s_0004s_0000s_0000_excellent.png";
         document.getElementById("imgstatus1").style.visibility="visible";
     }else{
         document.getElementById("imgstatus1").src="imgs/sorry.png";
         document.getElementById("imgstatus1").style.visibility="visible";
     }
     if(ansTwo == formulaKelvinToCelcius(350)){
+	  document.getElementById("imgstatus2").src="imgs/Activity-9_0000s_0004s_0000s_0000_excellent.png";
         document.getElementById("imgstatus2").style.visibility="visible";
     }else{
         document.getElementById("imgstatus2").src="imgs/sorry.png";
         document.getElementById("imgstatus2").style.visibility="visible";
     }
     if(ansThree == formulaKelvinToCelcius(370)){
+        document.getElementById("imgstatus3").src="imgs/Activity-9_0000s_0004s_0000s_0000_excellent.png";
         document.getElementById("imgstatus3").style.visibility="visible";
     }else{
         document.getElementById("imgstatus3").src="imgs/sorry.png";
         document.getElementById("imgstatus3").style.visibility="visible";
     }
     if(ansFour == formulaCelciusToKelvin(27)){
+        document.getElementById("imgstatus4").src="imgs/Activity-9_0000s_0004s_0000s_0000_excellent.png";
         document.getElementById("imgstatus4").style.visibility="visible";
     }else{
         document.getElementById("imgstatus4").src="imgs/sorry.png";
